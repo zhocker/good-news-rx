26ad94d9d7dacf9b253968eb9415b29b5023f3c3
Tanawat Polsuwan <tanawat_pol@freewillsolutions.com>
2019-09-24 16:53:37 +0700

    Rewrite

---
+++ good-news/Extensions/URLRequest+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  URLRequest+Extension.swift
+//  good-news
+//
+//  Created by macbook on 9/24/19.
+//  Copyright © 2019 Tanawat Polsuwan. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct Resource<Item:Decodable> {
+    let url : URL
+}
+
+extension URLRequest {
+    
+    static func load<Item>(resource : Resource<Item>) -> Observable<Item?> {
+        return Observable.from([resource.url]).flatMap({ (url) -> Observable<Data> in
+            let request = URLRequest(url: url)
+            return URLSession.shared.rx.data(request: request)
+        }).map({ (data) -> Item? in
+            return try? JSONDecoder().decode(Item.self, from: data)
+        }).asObservable()
+    }
+    
+}
